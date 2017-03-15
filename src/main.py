3fad654a4c3fe209cc1f307b715d7cbdc9a6fbd8
Andrej Nano <nano@andrejnano.sk>
2017-03-15 16:34:51 +0100

    Add kivy zakladne spustenie

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
+
+'''
+
+SimpleCalc
+2. projekt IVS / 2017
+@ FIT VUT, BRNO
+
+Peter Marko
+Stanislav Mechl
+Andrej Nano
+
+'''
 
 from kivy.app import App
 from kivy.uix.button import Button
