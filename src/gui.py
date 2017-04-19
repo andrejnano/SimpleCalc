988b25e3c0cb37bc4360497c502f3d8a9da296c5
Andrej Nano <nano@andrejnano.sk>
2017-04-19 16:55:22 +0200

    solved merge conflict (merge from gui branch)

--- src/gui.py
+++
@@ -1,4 +0,0 @@
-'''
-sem by som chcel pripadne dat nastavenia GUI rozhrania
-potom importnut v hlavnom .py subore
-''' 
--- src/main.py
+++ src/main.py
@@ -1,38 +1,34 @@
 #!/bin/python
 
-'''
-SimpleCalc
-2. projekt IVS / 2017
-@ FIT VUT, BRNO
-
-Peter Marko
-Stanislav Mechl
-Andrej Nano
-''' 
-    
-import kivy
+# + ----------------------- +
+# |   SimpleCalc            |
+# |   2. projekt IVS / 2017 |
+# |   @ FIT VUT, BRNO       |
+# |                         |
+# |   Peter Marko           |
+# |   Stanislav Mechl       |
+# |   Andrej Nano           |
+# + ----------------------- +   
 
 import mathFunc
-
+import kivy
 from kivy.app import App
-
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
+
+
 class CalcGridLayout(GridLayout):
-        # self.display.text = 
     def calculate(self,calculation):
         try:
             self.display.text = str(eval(calculation))
         except Exception:
             self.display.text = 'Error'
 
-
 class CalculatorApp(App):
-
     def build(self):
-        
         return CalcGridLayout()
 
-calcApp = CalculatorApp()
-calcApp.run()
+if __name__ == "__main__":
+    CalculatorApp().run()
+
