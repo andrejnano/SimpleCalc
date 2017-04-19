4963cea7cd1a9bf740a95e07a7122af74da1e7f7
Stanislav Mechl <smechl@seznam.cz>
2017-04-19 10:56:16 +0200

    Vytvoreny prototypy funkci

--- src/mathFunc.py
+++ src/mathFunc.py
@@ -1,5 +1,23 @@
 def plus(x,y):
-    return x+y
+    return 0
     
 def minus(x,y):
-    return x-y
+    return 0
+
+def mul(x,y):
+    return 0
+    
+def div(x,y):
+    return 0
+
+def pow(x,y):
+    return 0
+    
+def sqrt(x,y):
+    return 0
+
+def fact(x):
+    return 0
+
+def log(x):
+    return 0
