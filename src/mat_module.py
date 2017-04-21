41859769c5b06197c3b17bc0258127dc0a07ad9d
Peter Marko <xmarko15@stud.fit.vutbr.cz>
2017-04-21 22:32:06 +0200

    root function calculation

--- src/mat_module.py
+++ src/mat_module.py
@@ -30,11 +30,11 @@ def div ( A , B ):
     else:
         return A / B
 
-def sqrt ( A ):
-    if ( not (isinstance(A, numbers.Real) )):
-        raise ValueError('Argument passed to function sqrt is not float')
+def root ( A,B ):
+    if ( not (isinstance(A, numbers.Real) and isinstance(B, numbers.Real))):
+        raise ValueError('Argument passed to function root is not float')
     else:
-        return math.sqrt(A)
+        return B**(1.0/A)
 
 
 def factorial( A ):
@@ -50,6 +50,30 @@ def factorial( A ):
         A -= 1
     return float(result) 
 
+def sin( A ):
+    if ( not (isinstance(A, numbers.Real) )):
+        raise ValueError('Argument passed to function sin is not float')
+    else:
+        return math.sin(A)
+
+def cos( A ):
+    if ( not (isinstance(A, numbers.Real) )):
+        raise ValueError('Argument passed to function cos is not float')
+    else:
+        return math.cos(A)
+
+def tan( A ):
+    if ( not (isinstance(A, numbers.Real) )):
+        raise ValueError('Argument passed to function tan is not float')
+    else:
+        return math.tan(A)
+
+def log( A ):
+    if ( not (isinstance(A, numbers.Real) )):
+        raise ValueError('Argument passed to function log is not float')
+    else:
+        return math.log(A)
+
 def isnum( char ):
     if (ord(char) > 46 and ord(char) < 58):
         return 1
@@ -59,8 +83,8 @@ def isnum( char ):
 # funkcia ktoru treba spustit pre pocitanie
 def evaluate( string ):
     while( string.find("(") != -1 ):
-        l_idx = string.rfind("(")
-        r_idx = string.find(")")
+        l_idx = lbidx(string)
+        r_idx = rbidx(string)
         string = string[:l_idx] +calculate(string[l_idx+1:r_idx]) + string[r_idx+1:]
     result = calculate(string)
     if(result[0] is "+"):
@@ -79,17 +103,37 @@ def find_nan( string ):
         i += 1
     return len(string)
 
+def trigonFunc(string, sign,func):
+    while ( string.find(sign) > -1):
+         lidx = string.find(sign)
+         ridx = lidx + 4 + find_nan(string[lidx+4:])
+         string = string[:lidx] + (str("%.12f" % func(float(string[lidx+3:ridx]))).rstrip('0').rstrip('.')) + string[ridx:]
+         break
+    print(string)
+    
+    return string
+
 def calcFactorSqrt( string ):
     sqr_sindex = string.rfind("\xe2\x88\x9a")
     while (sqr_sindex >= 0):
         if (isnum(string[sqr_sindex-1])):
-            string = string[:sqr_sindex] + "*" + string[sqr_sindex:]
+            string = string[:sqr_sindex]  + string[sqr_sindex:]
             sqr_sindex += 1
-        sqr = string[sqr_sindex + 3:]
+        sqr = string[sqr_sindex + 2:]
         sqr_eindex = find_nan(sqr) 
         sqr = sqr[:sqr_eindex]
-        sqr = "%g" % sqrt(float(sqr))
-        string =  string[:sqr_sindex] + sqr + string[sqr_sindex + sqr_eindex + 3:]
+        second = sqr
+        tmpstr = string[:sqr_sindex -1]
+        sqr_sidx = find_nan(tmpstr[::-1])
+        print(sqr_sidx)
+        first = string[sqr_sindex-sqr_sidx-1:sqr_sindex-1]
+        print(first)
+        if(first is ""):
+            first = "2"
+        sqr = str("%.12f" % root(float(first),float(second))).rstrip('0').rstrip('.')
+        print"this is it"
+        print(string[:sqr_sindex-sqr_sidx-2])
+        string =  string[:sqr_sindex-sqr_sidx - 1] + sqr + string[sqr_sindex + sqr_eindex:]
         sqr_sindex = string.rfind("\xe2\x88\x9a")
 
     fac_eindex = string.find("!")
@@ -97,9 +141,17 @@ def calcFactorSqrt( string ):
         fac = string[:fac_eindex]
         fac_sindex = find_nan(fac[::-1]) 
         fac = fac[fac_eindex - fac_sindex:fac_eindex]
-        fac = "%g" % factorial(float(fac))
+        fac = str("%.12f" % factorial(float(fac))).rstrip('0').rstrip('.')
         string =  string[:fac_eindex - fac_sindex] + fac + string[fac_eindex + 1:]
         fac_eindex = string.find("!")
+
+    string = trigonFunc(string,"sin",sin)
+    string = trigonFunc(string,"cos",cos)
+    string = trigonFunc(string,"tan",tan)
+    string = trigonFunc(string,"log",log)
+    #  break
+    # while ( string.find("cos(") > -1):
+    # while ( string.find("tan(") > -1):
     return string
 
 def determineSign( string ):
@@ -119,6 +171,33 @@ def determineSign( string ):
         string = "+" + string[i:]
     return string
 
+def lbidx( string ):
+    l_idx = 0
+    i = 0
+    if (string.find("(") == -1 or string.find(")") == -1):
+        return -1 
+    for c in string:
+        if (c is "("):
+            l_idx = i
+        if(c is ")"):
+            break
+        i+=1
+    return l_idx
+
+
+def rbidx( string ):
+    l_idx = 0
+    i = 0
+    if (string.find("(") == -1 or string.find(")") == -1):
+        return -1 
+    for c in string:
+        if (c is "("):
+            l_idx = i
+        if(c is ")"):
+            break
+        i+=1
+    return i
+
 def calcSum( string ):
     # nacitaj prve cislo
     # vyhodnot znamienko
@@ -132,7 +211,7 @@ def calcSum( string ):
         string = determineSign(string)
         i = 1 + find_nan(string[1:])
         second =  string[:i]
-        string = "%g" % add(float(first),float(second)) + string[i:]
+        string = str("%.12f" %  add(float(first),float(second)) + string[i:]).rstrip('0').rstrip('.')
     return string
 
 def calcBasicOperations( string, sign, operation ):
@@ -171,7 +250,7 @@ def calcBasicOperations( string, sign, operation ):
         if(SNum is ""):
             break
 
-        result = "%g" % operation(float(FNum),float(SNum))
+        result = str("%.12f" % operation(float(FNum),float(SNum))).rstrip('0').rstrip('.')
         if(result[0] is not "-"):
             string = string[:FNumIdx_ll] +"+"+ result + string[SNumIdx_l + SNumIdx_r:]
         else:
