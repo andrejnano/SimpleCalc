9b7b8c57e623fc53e47591d8f86e54f71d9e124e
Andrej Nano <xnanoa00@vutbr.cz>
2017-04-22 14:32:47 +0200

    Update main.py (missing window import)

--- src/main.py
+++ src/main.py
@@ -67,7 +67,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
-
+from kivy.uix.window import Window
 
 class CalcGridLayout(GridLayout):
 
