97985410455d4cf5aa6a3bbf2dd4a2daf3837483
Andrej Nano <nano@andrejnano.sk>
2017-04-22 13:24:41 +0200

    simplified calculator.kv

--- src/main.py
+++ src/main.py
@@ -10,20 +10,39 @@
 # |   Andrej Nano           |
 # + ----------------------- +   
 
+# python modules
+import platform
+
 import kivy
+kivy.require('1.9.0')
+
+# kivy configs
 from kivy.config import Config
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'borderless', 0)
+
+#podmienka kvoli retina displeju na macu
+#automaticky sa zdvojnasobuje velkost okna.
+#na win. a linux je to ok
+
+if platform.system() == "Darwin":
+    Config.set('graphics', 'height', 225)
+    Config.set('graphics', 'width', 225)
+else:
+    Config.set('graphics', 'height', 450)
+    Config.set('graphics', 'width', 450)
+
+Config.write()
+
+# kivy modules
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
 from kivy.core.window import Window
 
-
-# kivy configs
-Config.set('graphics', 'resizable', 0)
-Config.set('graphics', 'height', 900)
-Config.set('graphics', 'width', 600)
-Config.set('graphics', 'borderless', 0)
-Config.write()
 
 class CalcGridLayout(GridLayout):
     def calculate(self,calculation):
@@ -33,9 +52,40 @@ class CalcGridLayout(GridLayout):
             self.display.text = 'Error'
 
 class CalculatorApp(App):
+   
+    # vykreslenie
     def build(self):
-        return CalcGridLayout()
+        self.grid = CalcGridLayout()
+        return self.grid 
+    
+    def numpress(self, num):
+        print("numpressed")
+    def oppress(self, op):
+        print("opppressed")
+ 
+    # pomocne okno
+    def dochelp(self, *args):
+        box = BoxLayout();
+        box.add_widget(Label(text="""Lorem ipsum dolor sit amet, \n consectetur
+        \nVestibulum dictum \nvelit eu mattis bibendum.
+        """, size=(600, 400), halign='center'))
 
+        button1 = Button(text="Zatvorit okno", size_hint=(None, None),
+                size=(700, 150))
+        box.add_widget(button1)
+        
+        popup = Popup(title='Help', content=box, size_hint=(None, None),
+                size=(750, 900), background_normal='', background_color=(0, 0, 0, 0.5))
+        
+        button1.bind(on_press=popup.dismiss)
+        popup.open()
+
+
+
+
+calc_inst = CalculatorApp()
+
+## auto run 
 if __name__ == "__main__":
-    CalculatorApp().run()
+    calc_inst.run()
 
