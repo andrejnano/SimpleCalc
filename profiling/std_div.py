4498d8e800b02ee663d3eba284f1b26698144953
Peter Marko <xmarko15@stud.fit.vutbr.cz>
2017-04-23 08:14:13 +0200

    created program for calculating standard division

---
+++ profiling/std_div.py
@@ -0,0 +1,26 @@
+#!/bin/python
+import sys
+sys.path.append('../src')
+import mat_module
+import cProfile
+
+def std_div():
+    data = []
+    N="0"
+    sum = "0"
+    for line in sys.stdin:
+        data.append(line[:-1])
+        sum = mat_module.evaluate(line[:-1]+"+"+sum)
+        N = mat_module.evaluate("1"+"+"+N)
+    avg = mat_module.evaluate("("+sum+")/"+N)
+    j="0"
+    sum = "0"
+    for i in data:
+        sum = mat_module.evaluate(sum+"+"+"("+data[int(j)] +"-"+ avg+")"+"^2")
+        j = mat_module.evaluate("1+"+j)
+    string = '\xe2\x88\x9a('+sum+"/(" + N + "-1))"
+    print(string)
+    S = mat_module.evaluate(  string  )
+    print(S)
+    return S
+cProfile.run("std_div()")
